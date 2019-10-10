753ea6177880cc708d7adb242b8f600ba9043a3e
Maho Morichika <nat@magicnat.com>
2019-10-09 22:30:48 -0400

    log: allow fmt only log

--- src/log.h
+++ src/log.h
@@ -3,26 +3,26 @@
 #include <stdio.h>
 #include <time.h>
 
-#define log_info(fmt, ...) log("INFO ", fmt, __VA_ARGS__)
-#define log_notice(fmt, ...) log("NOTE ", fmt, __VA_ARGS__)
-#define log_warn(fmt, ...) log("WARN ", fmt, __VA_ARGS__)
-#define log_error(fmt, ...) log("ERROR", fmt, __VA_ARGS__)
-#define log_fatal(fmt, ...) log("FATAL", fmt, __VA_ARGS__)
+#define log_info(fmt, ...) log("INFO ", fmt, ## __VA_ARGS__)
+#define log_notice(fmt, ...) log("NOTE ", fmt, ## __VA_ARGS__)
+#define log_warn(fmt, ...) log("WARN ", fmt, ## __VA_ARGS__)
+#define log_error(fmt, ...) log("ERROR", fmt, ## __VA_ARGS__)
+#define log_fatal(fmt, ...) log("FATAL", fmt, ## __VA_ARGS__)
 
 #ifndef DIST_SILENCE
-#define log(log_level, fmt, ...) fprintf(stderr, "[%ld " log_level "] %s: " fmt, (long) time(NULL), __PRETTY_FUNCTION__, __VA_ARGS__)
+#define log(log_level, fmt, ...) fprintf(stderr, "[%ld " log_level "] %s: " fmt, (long) time(NULL), __PRETTY_FUNCTION__, ## __VA_ARGS__)
 #else
 #define log(log_level, fmt, ...)
 #endif // DIST_SILENCE
 
 #ifdef DIST_DEBUG
-#define log_debug(fmt, ...) log("DEBUG", fmt, __VA_ARGS__)
+#define log_debug(fmt, ...) log("DEBUG", fmt, ## __VA_ARGS__)
 #else
 #define log_debug(fmt, ...)
 #endif // DIST_DEBUG
 
 #ifdef DIST_LOGIC_DEBUG
-#define log_logic(fmt, ...) log("LOGIC", fmt, __VA_ARGS__)
+#define log_logic(fmt, ...) log("LOGIC", fmt, ## __VA_ARGS__)
 #else
 #define log_logic(fmt, ...)
 #endif // DIST_LOGIC_DEBUG
