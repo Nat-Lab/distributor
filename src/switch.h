a4d8bb30ff9996ba7f3735320f90b329f1401369
Maho Morichika <nat@magicnat.com>
2019-10-09 22:31:06 -0400

    switch: impl changes

--- src/switch.h
+++ src/switch.h
@@ -4,29 +4,54 @@
 #include "fdb.h"
 #include <stdint.h>
 #include <map>
+#include <unordered_map>
 #include <vector>
+#include <mutex>
 
 namespace distributor {
 
 class Switch {
-public:
-    Switch ();
-
+protected:
     // Plug a port into a network.
-    bool Plug (net_t network, port_t port);
+    void Plug (net_t network, port_t port);
 
-    // Unplug a port from a network.
-    bool Unplug (net_t network, port_t port);
+    // Unplug a port from the network. Return true if removed, false otherwise.
+    bool Unplug (port_t port);
+
+    // Foward an ethernet frame.
+    void Forward (port_t src_port, const uint8_t *frame, size_t size);
+
+    // Flush FDB entries for a port.
+    void FlushFdb (port_t port);
+
+    // Send an ethernet frame to port. Should be implement by distributor. 
+    // Return -1 on error, return bytes written on success.
+    virtual ssize_t Send (port_t dst, const uint8_t *frame, size_t size) = 0;
+
+    typedef std::map<port_t, net_t> portsmap_t;
+    typedef std::unordered_multimap<net_t, port_t> netsmap_t;
+    typedef std::map<net_t, Fdb> fdbsmap_t;
 
 private:
+    // don't be confused by portsmap_t and netsmap_t. The key type in portsmap_t
+    // is port_t, but value type is net_t, thus GetNetsByPort return portsmap_t.
+    portsmap_t::const_iterator GetNetByPort (port_t port) const;
+    netsmap_t::const_iterator GetPortsByNet (net_t net) const;
+
+    // Flush FDB, private version. No write mutex.
+    void FlushFdbPriv (net_t net, port_t port);
+
     // port to network mapping
-    std::map<port_t, net_t> _ports;
+    portsmap_t _ports;
 
     // network to ports mapping
-    std::multimap<net_t, port_t> _nets;
+    netsmap_t _nets;
 
     // network to fdb mapping
-    std::map<net_t, Fdb> _fdbs;
+    fdbsmap_t _fdbs;
+
+    // mutex for maps
+    std::mutex _maps_write_mtx;
 };
 
 }
