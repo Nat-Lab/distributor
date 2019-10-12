0ce2bba18b1c16c03505f5ed5908c14fde2e0ae3
Maho Morichika <nat@magicnat.com>
2019-10-12 02:19:57 -0400

    vars: new vars

--- src/vars.h
+++ src/vars.h
@@ -4,16 +4,24 @@
 // mac address ageing time in second in fdb. 
 #ifndef DIST_FDB_AGEING 
 #define DIST_FDB_AGEING 300
-#endif 
+#endif // DIST_FDB_AGEING
 
 // interval of the keepalive.
-#ifndef DIST_FDB_AGEING
+#ifndef DIST_UDP_KEEPALIVE
 #define DIST_UDP_KEEPALIVE 60
-#endif
+#endif // DIST_UDP_KEEPALIVE
 
 // remove clients after N times they failed to respond to keepalive
 #ifndef DIST_UDP_RETRIES
 #define DIST_UDP_RETRIES 3
 #endif
+
+#ifndef DIST_WOROKER_READ_BUFSZ
+#define DIST_WOROKER_READ_BUFSZ 65536
+#endif // DIST_WOROKER_READ_BUFSZ
+
+#ifndef DIST_CLIENT_SEND_BUFSZ
+#define DIST_CLIENT_SEND_BUFSZ 65536
+#endif // DIST_CLIENT_SEND_BUFSZ
 
 #endif // DIST_VARS_H
